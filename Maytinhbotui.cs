82b4849ea8eb2a932bf4378a820ffbf22a536728
NTCandy-dev <trungmuoi78@gmail.com>
2024-12-13 22:54:47 +0700

    Update Maytinhbotui.cs

--- Maytinhbotui.cs
+++ Maytinhbotui.cs
@@ -1,177 +1,300 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Menu
 {
-    public partial class Maytinhbotui : Form
+    public partial class maytinhtay : Form
     {
-        private String input = "";
-        private double sodau = 0;
         private String pheptoan = "";
-        private String hienthi_ketqua = "";
-        public Maytinhbotui()
+        private double sothunhat = 0;
+        private bool dieukien = true;
+        public maytinhtay()
         {
             InitializeComponent();
         }
 
-        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
-        private void button_Click(object sender, PaintEventArgs e)
+
+        private void richTextBox1_ForeColorChanged(object sender, EventArgs e)
         {
-            Button btn = (Button)sender;
-            input += btn.Text;
-            hienthi_ketqua += btn.Text;
-            hienthi.Text = hienthi_ketqua;
             
         }
-        private void operator_Click(object sender, EventArgs e)
-        {
-            Button btn = (Button)sender;
-            if (input != "")
-            {
-                sodau = double.Parse(input);
-                input = "";
-            }
-            pheptoan = btn.Text;
-            hienthi_ketqua += " " + btn.Text + " "; 
-            hienthi.Text = hienthi_ketqua;
-        }
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
 
-        }
-
-        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        private void hienthi_KeyPress(object sender, KeyPressEventArgs e)
         {
             e.Handled = true;
         }
 
-        private void clear_Click(object sender, EventArgs e)
+        private void maytinhtay_ForeColorChanged(object sender, EventArgs e)
         {
-            hienthi.Clear();
-            sodau = 0;
-            input = "";
-            pheptoan = "";
+            hienthi.ForeColor = Color.Blue;
         }
 
-        private void Maytinhbotui_Load(object sender, EventArgs e)
+        private void maytinhtay_Load(object sender, EventArgs e)
         {
-
-        }
-
-        private void equal_Click(object sender, EventArgs e)
-        {
-            double sotieptheo;
-            if(double.TryParse(input, out sotieptheo))
-            {
-                double result = 0;
-                switch (pheptoan)
-                {
-                    case "+":
-                        result = sodau + sotieptheo;
-                        break;
-                    case "-":
-                        result = sodau - sotieptheo;
-                        break;
-                    case "*":
-                        result = sodau * sotieptheo;
-                        break;
-                    case "/":
-                        if(sotieptheo == 0 )
-                        {
-                            MessageBox.Show("Không thể chia cho 0");
-                            return;
-                        }
-                        else
-                        {
-                            result = sodau / sotieptheo;
-                        }
-                        break;    
-                }
-                hienthi_ketqua += " = " + result.ToString(); 
-                hienthi.Text = hienthi_ketqua;
-                input = result.ToString(); 
-                pheptoan = "";
-            }
-        }
-
-        private void cong_Click(object sender, EventArgs e)
-        {
-            hienthi.Text += "+";
+            hienthi.SelectionStart = hienthi.Text.Length;
+            hienthi.ScrollToCaret();
         }
 
         private void mot_Click(object sender, EventArgs e)
         {
-            hienthi.Text += "1";
+            Button btn = (Button)sender;
+            if(hienthi.Text == "0" || dieukien)
+            {
+                hienthi.Clear();
+            }
+            dieukien = false;
+            hienthi.Text += btn.Text;
         }
 
         private void hai_Click(object sender, EventArgs e)
         {
-            hienthi.Text += "2";
+            Button btn = (Button)sender;
+            if (hienthi.Text == "0" || dieukien)
+            {
+                hienthi.Clear();
+            }
+            dieukien = false;
+            hienthi.Text += btn.Text;
         }
 
         private void ba_Click(object sender, EventArgs e)
         {
-            hienthi.Text += "3";
+            Button btn = (Button)sender;
+            if (hienthi.Text == "0" || dieukien)
+            {
+                hienthi.Clear();
+            }
+            dieukien = false;
+            hienthi.Text += btn.Text;
         }
 
         private void bon_Click(object sender, EventArgs e)
         {
-            hienthi.Text += "4";
+            Button btn = (Button)sender;
+            if (hienthi.Text == "0" || dieukien)
+            {
+                hienthi.Clear();
+            }
+            dieukien = false;
+            hienthi.Text += btn.Text;
         }
 
         private void nam_Click(object sender, EventArgs e)
         {
-            hienthi.Text += "5";
+            Button btn = (Button)sender;
+            if (hienthi.Text == "0" || dieukien)
+            {
+                hienthi.Clear();
+            }
+            dieukien = false;
+            hienthi.Text += btn.Text;
         }
 
         private void sau_Click(object sender, EventArgs e)
         {
-            hienthi.Text += "6";
+            Button btn = (Button)sender;
+            if (hienthi.Text == "0" || dieukien)
+            {
+                hienthi.Clear();
+            }
+            dieukien = false;
+            hienthi.Text += btn.Text;
         }
 
         private void bay_Click(object sender, EventArgs e)
         {
-            hienthi.Text += "7";
+            Button btn = (Button)sender;
+            if (hienthi.Text == "0" || dieukien)
+            {
+                hienthi.Clear();
+            }
+            dieukien = false;
+            hienthi.Text += btn.Text;
         }
 
         private void tam_Click(object sender, EventArgs e)
         {
-            hienthi.Text += "8";
+            Button btn = (Button)sender;
+            if (hienthi.Text == "0" || dieukien)
+            {
+                hienthi.Clear();
+            }
+            dieukien = false;
+            hienthi.Text += btn.Text;
         }
 
         private void chin_Click(object sender, EventArgs e)
         {
-            hienthi.Text += "9";
+            Button btn = (Button)sender;
+            if (hienthi.Text == "0" || dieukien)
+            {
+                hienthi.Clear();
+            }
+            dieukien = false;
+            hienthi.Text += btn.Text;
         }
 
         private void khong_Click(object sender, EventArgs e)
         {
-            hienthi.Text += "0";
+            Button btn = (Button)sender;
+            if (hienthi.Text == "0" || dieukien)
+            {
+                hienthi.Clear();
+            }
+            dieukien = false;
+            hienthi.Text += btn.Text;
+        }
+
+        private void cong_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if(!string.IsNullOrEmpty(pheptoan) || dieukien)
+            {   
+                pheptoan = btn.Text;
+                hienthi.Text = pheptoan;
+                return;
+            }
+            else if (string.IsNullOrEmpty(hienthi.Text))
+                sothunhat = 0;
+            else
+            {
+                if(sothunhat != 0 || string.IsNullOrEmpty(pheptoan))
+                    bang.PerformClick();
+                else 
+                    sothunhat = double.Parse(hienthi.Text);
+            }
+            pheptoan = btn.Text;
+            dieukien = true;
+            hienthi.Text += btn.Text;
         }
 
         private void tru_Click(object sender, EventArgs e)
         {
-            hienthi.Text += "-";
+            Button btn = (Button)sender;
+            if (!string.IsNullOrEmpty(pheptoan) && dieukien)
+            {
+                pheptoan = btn.Text;
+                hienthi.Text = pheptoan;
+                return;
+            }
+
+            else if(string.IsNullOrEmpty(hienthi.Text))
+                sothunhat = 0;
+            else
+            {
+                if (sothunhat != 0 && !string.IsNullOrEmpty(pheptoan))
+                    bang.PerformClick();
+                else
+                    sothunhat = double.Parse(hienthi.Text);
+            }
+            pheptoan = btn.Text;
+            dieukien = true;
+            hienthi.Text += btn.Text;
         }
 
         private void nhan_Click(object sender, EventArgs e)
         {
-            hienthi.Text += "*";
+            Button btn = (Button)sender;
+            if (!string.IsNullOrEmpty(pheptoan) && dieukien)
+            {
+                pheptoan = btn.Text;
+                hienthi.Text = pheptoan;
+                return;
+            }
+
+            else if(string.IsNullOrEmpty(hienthi.Text))
+                sothunhat = 0;
+            else
+            {
+                if (sothunhat != 0 && !string.IsNullOrEmpty(pheptoan))
+                    bang.PerformClick();
+                else
+                    sothunhat = double.Parse(hienthi.Text);
+            }
+            pheptoan = btn.Text;
+            dieukien = true;
+            hienthi.Text += btn.Text;
         }
 
         private void chia_Click(object sender, EventArgs e)
         {
-            hienthi.Text += "/";
+            Button btn = (Button)sender;
+            if (!string.IsNullOrEmpty(pheptoan) && dieukien)
+            {
+                pheptoan = btn.Text;
+                hienthi.Text = pheptoan;
+                return;
+            }
+
+            else if (string.IsNullOrEmpty(hienthi.Text))
+                sothunhat = 0;
+            else
+            {
+                if (sothunhat != 0 && !string.IsNullOrEmpty(pheptoan))
+                    bang.PerformClick();
+                else
+                    sothunhat = double.Parse(hienthi.Text);
+            }
+            pheptoan = btn.Text;
+            dieukien = true;
+            hienthi.Text += btn.Text;
+        }
+
+        private void bang_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double sothuhai = double.Parse(hienthi.Text);
+                switch (pheptoan)
+                {
+                    case "+":
+                        hienthi.Text = (sothunhat + sothuhai).ToString();
+                        break;
+                    case "-":
+                        hienthi.Text = (sothunhat - sothuhai).ToString();
+                        break;
+                    case "*":
+                        hienthi.Text = (sothunhat * sothuhai).ToString();
+                        break;
+                    case "/":
+                        if(sothuhai == 0)
+                        {
+                            MessageBox.Show("Không thể chia cho 0","Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+                            return;
+                        }
+                        else
+                            hienthi.Text = (sothunhat / sothuhai).ToString();
+                            break;
+                }
+                sothunhat = double.Parse(hienthi.Text);
+                pheptoan = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Dữ liệu không hợp lệ","Thông báo",MessageBoxButtons.OKCancel,MessageBoxIcon.Error);
+                return;
+            }
+            
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            hienthi.Text = "0";
+            sothunhat = 0;
+            pheptoan = "";
         }
     }
 }
